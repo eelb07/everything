765628d9fa752c08ac035b4e4e1fc010f66e6453
seunga lee <106696686+eelb07@users.noreply.github.com>
2025-04-07 15:48:43 +0900

    ✅ test: profile 변경

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    name: "SeungaLee",
+    image: "/ava_flower.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "student",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "foutsouts2@gmail.com",
+    linkedin: "SeungaLee",
+    github: "SeungaLee",
     instagram: "",
   },
   projects: [
