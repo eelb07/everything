c48dd44fc41d869f2192ffaccb2dacb81f551883
seunga lee <106696686+eelb07@users.noreply.github.com>
2025-04-24 11:12:14 +0900

    🐛 fix: change profile

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Seunga Lee",
-    image: "/ava_flower.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/ts.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
     bio: "",
     email: "foutsouts2@gmail.com",
